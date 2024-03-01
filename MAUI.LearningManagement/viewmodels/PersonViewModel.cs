cca539e2ef1af95a4f4a4e03926f3c5bd0458a87
Jose <josebrouwer1@gmail.com>
2024-03-01 14:58:54 -0500

    Changed LMS from exe to Library. Can use LMS in MAUI.

--- MAUI.LearningManagement/viewmodels/PersonViewModel.cs
+++ MAUI.LearningManagement/viewmodels/PersonViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LMS.Models;
 
 namespace MAUI.LearningManagement.viewmodels
 {
     class PersonViewModel
     {
-        //private Person underlyingPerson;
+        private Person underlyingPerson;
     }
 }
